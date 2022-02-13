9950a3f9d9457253c5710d7ebb0f70aca478f281
vgj24 <vrushlijoshi24@gmail.com>
2022-02-13 16:05:57 +0530

    Creating Person Data

---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProject10FebAssgn
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+       
+        public Person(string ssn, string name, string addr, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+        }
+
+
+    }
+
+
+}
